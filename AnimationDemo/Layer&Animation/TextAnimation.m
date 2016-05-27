33d0c2f24372978cd211792482638c42ef8d6b7f
guibin <15102126412@163.com>
2016-05-27 14:51:04 +0800

    change textAnimatin 's Font

--- AnimationDemo/Layer&Animation/TextAnimation.m
+++ AnimationDemo/Layer&Animation/TextAnimation.m
@@ -26,7 +26,7 @@
 {
     self.view.layer.contents = (__bridge id)[UIImage imageNamed:@"xingkong.jpeg"].CGImage;
 
-    self.textView = [[DrawTextView alloc]initWithFrame:CGRectMake(CGRectGetMidX(self.view.frame)- 100, 100, 200, 200) String:@"A12312312" font:[UIFont fontWithName:@"DBLCDTempBlack" size:60]];
+    self.textView = [[DrawTextView alloc]initWithFrame:CGRectMake(CGRectGetMidX(self.view.frame)- 100, 100, 200, 200) String:@"A12312312" font:[UIFont systemFontOfSize:60]];
     [self.view addSubview:self.textView];
 }
 
